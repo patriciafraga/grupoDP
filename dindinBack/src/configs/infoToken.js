9eb9aafcc4b0d4eb81c7b8b939d05fb9b7b6ad17
patriciafraga <patricia.f.fraga@gmail.com>
2023-11-03 11:03:35 -0300

    exclusão dindinBack

--- dindinBack/src/configs/infoToken.js
+++
@@ -1,1 +0,0 @@
-module.exports = `des@fio Cub@$`;
--- dindinBack/src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const porta = 3000;
-const rota = require('./rotas/rota');
-app.use(rota);
-app.use(express.json());
-
-app.listen(porta, () => console.log(`Rodando em http://localhost:${porta}`));
